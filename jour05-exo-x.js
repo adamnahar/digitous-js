91b225b902f4d3e81a1c712929ce94607910d3c4
Mohammed <adamnahar3020@gmail.com>
2020-12-17 10:16:12 +0100

    jour05

---
+++ jour05-exo-x.js
@@ -0,0 +1,36 @@
+// 01 calculator
+function calculator ( n1 , n2 , operator){
+    switch (operator){
+        case "+" : {
+            return number1 + number2 ;
+        }
+        case "-" :{
+        
+        } return number1 - number2;
+        case "*" : {
+            return number1 * number2;
+        }
+        case "/" :{
+            return number1 / number2;
+        }
+        case "%" : {
+            return number1 % number2;
+        }
+        default : {
+            console.log(`${operator}`)
+        }
+    }
+    
+    
+    }
+}
+
+
+// 02 table
+
+
+// 03 separate table
+
+
+// 04 guess
+
